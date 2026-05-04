5f0ac98014aed89f01a797de3497db6cb2faa896
miyaajd <ehdal1032@gamil.com>
2026-05-04 19:00:32 +0900

    메타데이터설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,36 @@ import Footer from "../components/layout/Footer";
 import Header from "../components/layout/Header";
 import "./globals.css";
 
+// 메타데이터 설정
+export const metadata = {
+  title: "MIYAAJD",
+  description: "Personal Archive",
+
+  icons: {
+    icon: [
+      {
+        url: "/icons/favicon/web-app-manifest-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        url: "/icons/favicon/web-app-manifest-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+    apple: "/icons/favicon/apple-touch-icon.png",
+  },
+  manifest: "/manifest.json",
+
+  // 아이폰 대응
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: "MIAAJD",
+  },
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
